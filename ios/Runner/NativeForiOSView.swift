24ed0ef5525b63e20b8fe6b97f04574414c64d97
zhouyubin <zhouyubinwangyi@163.com>
2023-09-14 21:32:44 +0800

    111

--- ios/Runner/NativeForiOSView.swift
+++ ios/Runner/NativeForiOSView.swift
@@ -30,13 +30,54 @@ class NativeForiOSView: NSObject,FlutterPlatformView {
         return _view
     }
     func createNativeView(view _view: UIView){
-        _view.backgroundColor = .orange
-        let lab:UILabel = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 60))
+        self.setUpUI(_view)
+       }
+   
+}
+extension NativeForiOSView{
+    func setUpUI(_ view: UIView){
+        var width = UIScreen.main.bounds.size.width
+        view.backgroundColor = .orange
+        let lab:UILabel = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: width, height: 60))
         lab.text = "这是一个iOS 原生View:==" + self.text
         lab.textColor = .white
-        _view.addSubview(lab)
+        view.addSubview(lab)
+        let btn:UIButton = UIButton.init(frame: CGRect.init(x: 20, y: CGRectGetMaxY(lab.frame)+20, width: width - 40, height: 45))
+        btn.addTarget(self, action: #selector(btnClick(_ :)), for: .touchUpInside)
+        btn.setTitle("iOS 按钮", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = .blue
+        view.addSubview(btn)
+    }
+    @objc func btnClick(_ btn: UIButton){
+        let alert: UIAlertController = UIAlertController(title: "温馨提示", message: "我是一个原生的弹窗", preferredStyle: .alert)
+        let action1 = UIAlertAction.init(title: "取消", style: .cancel) { a in
+            print("iOS取消")
+        }
+        alert.addAction(action1)
+        self.getCurrentVc().present(alert, animated: true)
+        
+    }
+}
+extension NativeForiOSView{
+    public func getCurrentVc() -> UIViewController{
+        let rootVc = UIApplication.shared.keyWindow?.rootViewController
+        let currentVc = getCurrentVcFrom(rootVc!)
+        return currentVc
        }
-    
-    
-
+    private func getCurrentVcFrom(_ rootVc:UIViewController) -> UIViewController{
+     var currentVc:UIViewController
+     var rootCtr = rootVc
+     if(rootCtr.presentedViewController != nil) {
+       rootCtr = rootVc.presentedViewController!
+     }
+     if rootVc.isKind(of:UITabBarController.classForCoder()) {
+       currentVc = getCurrentVcFrom((rootVc as! UITabBarController).selectedViewController!)
+     }else if rootVc.isKind(of:UINavigationController.classForCoder()){
+       currentVc = getCurrentVcFrom((rootVc as! UINavigationController).visibleViewController!)
+     }else{
+       currentVc = rootCtr
+     }
+     return currentVc
+    }
 }
